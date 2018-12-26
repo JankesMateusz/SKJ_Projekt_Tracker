01f82177fb098a4b9d62b65d39ee8b75c89b5eca
unknown <jankesmateusz@gmail.com>
2018-12-26 11:53:17 +0100

    project reoriented to multithreaded server

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,20 @@
 public class Main {
 
     public static void main(String... args)throws Exception{
-        Tracker tracker = new Tracker();
-        tracker.start();
+
+        String ip = "";
+        int port = 0;
+
+        if(args.length > 0){
+            ip = args[0];
+            if(args.length > 1){
+                port = Integer.parseInt(args[1]);
+            }
+        }
+
+        Tracker tracker = new Tracker(ip, port);
+        ConnectionServer connectionServer = new ConnectionServer(tracker);
+        connectionServer.initialize();
+
     }
 }
